11d55d7fb2605800e266b8a6f272c6e233937c99
Nicole Janosi <janosin@umich.edu>
2017-12-06 23:27:44 -0500

    tumblr cache done

--- tumblr_main.py
+++ tumblr_main.py
@@ -1,6 +1,8 @@
 import oauth2
 import pytumblr
 import time
+import json
+import sqlite3
 
 client = pytumblr.TumblrRestClient(
 	'zQYemGVerUvczk9HCM7kmjBlX3EWL5b7Va1Wi0hEbNCJMKglqZ',
@@ -8,6 +10,16 @@ client = pytumblr.TumblrRestClient(
 	'4veWODwU9G2BNUYGC41uGHcLZvTzAMooIm7rMTqoL92MCIsCIk',
 	'yZv0Yj3ueXTkZJMxUGSzcgbsHarjcXd9yiqHrDrREN714QYYHR'
 )
+
+CACHE_FNAME = "206_Final_Project.json"
+# Put the rest of your caching setup here:
+try:
+	cache_file = open(CACHE_FNAME,'r')
+	cache_contents = cache_file.read()
+	cache_file.close()
+	CACHE_DICTION = json.loads(cache_contents)
+except:
+	CACHE_DICTION = {}
 
 
 def convert_timestamp (x):
@@ -17,17 +29,69 @@ def convert_timestamp (x):
 
 
 
-def tum_likes():
-	tum_likes_data = client.likes(limit = 50)['liked_posts']
-	for x in tum_likes_data:
-		like_time = (convert_timestamp(x['liked_timestamp']))
-		like_data_tup = (x['id'], like_time, x['summary'].strip())
-		print (like_data_tup)
+def tum_posts(blog):
+	 
 	
+	
+	if blog in CACHE_DICTION:
+		
+		#print("Tumblr data was in the cache")
+		tum_results = CACHE_DICTION[blog] #uses data already in cache
+		return tum_results
+	
+	else:
+		
+		#print("fetching tumblr data")
+		tum_posts_results = list()
 
-# print(client.posts('nicole-is-rad.tumblr.com', limit=50))
+		for x in client.posts(blog, limit=100, offset = 0)['posts']:
+			tum_posts_results.append(x)
+		
+		for x in client.posts(blog, limit=100, offset = 21)['posts']:
+			tum_posts_results.append(x)
+
+		for x in client.posts(blog, limit=100, offset = 42)['posts']:
+			tum_posts_results.append(x)
+
+		for x in client.posts(blog, limit=100, offset = 63)['posts']:
+			tum_posts_results.append(x)
+
+		for x in client.posts(blog, limit=100, offset = 84)['posts']:
+			tum_posts_results.append(x)
+		
+		CACHE_DICTION[blog] = tum_posts_results
+		cache_file = open(CACHE_FNAME, 'w') #then writes it to cache file
+		cache_file.write(json.dumps(CACHE_DICTION)+ "\n")
+		cache_file.close()
+		tum_results = tum_posts_results
+		return tum_results
+	
 
 def main():
-	tum_likes()
+
+	tum_data = tum_posts('nicole-is-rad.tumblr.com')
 	
+	conn = sqlite3.connect("206FinalProject.sqlite")
+	cur = conn.cursor()
+
+	cur.execute('DROP TABLE IF EXISTS Tumblr')
+	cur.execute('CREATE TABLE Tumblr (post_id NUMBER UNIQUE, day_of_week TEXT, time TEXT)')
+	
+	for x in tum_data:
+		
+		tum_id = (x['id'])
+		tum_date = convert_timestamp(x['timestamp']).split()
+		tum_day = tum_date[0]
+		tum_time = tum_date[2]+ " " + tum_date[3]
+		tum_tup = (tum_id, tum_day, tum_time)
+		
+		cur.execute('INSERT INTO Tumblr (post_id, day_of_week, time) VALUES (?,?,?)', tum_tup)
+	
+	conn.commit()		
+	
+	
+	
+	
+	
+	conn.close()
 main()
